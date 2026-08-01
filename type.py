94114ddb9cecd08cfc6aa542fd8dbd9a0ad9d4dd
EmyCodez <130977482+EmyCodez@users.noreply.github.com>
2026-08-01 16:19:28 +0400

    Data types in Python

---
+++ type.py
@@ -0,0 +1,22 @@
+a = 10
+print (type(a))
+b = 10.5
+print (type(b))
+c = 2 + 4j
+print (type(c))
+a = True
+print (type(a))
+b = None
+print (type(b))
+# Examples of sequence types
+print ("Sequence Types")
+str = "gfg"
+print (type(str))
+l = [10,20,30]
+print (type(l))
+t = (10, 20, 30)
+print (type(t))
+s = {10, 20, 30}
+print (type(s))
+d = {10 : "gfg", 20 : "ide"}
+print (type(d))
