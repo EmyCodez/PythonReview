bc7fe1bb07126da1afedb86ce78dfb6d01d0a179
EmyCodez <130977482+EmyCodez@users.noreply.github.com>
2026-08-20 13:33:12 +0400

    Use of end and sep in print()

---
+++ printFunction.py
@@ -0,0 +1,9 @@
+# Print space instead of newline
+print ("welcome", end =" ")
+print ("to GfG")
+# print newline
+print ()
+# specify separator as '-'
+print ("25", "08", "2025", sep = "-")
+print (10, 20, 30, sep = "+", end = " ")
+print (40, 50)
