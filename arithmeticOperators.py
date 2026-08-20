fcfe376a924f687a621f03831cfe8ef748d8db66
EmyCodez <130977482+EmyCodez@users.noreply.github.com>
2026-08-20 13:53:15 +0400

    Arithmetic and Logical Operators in Python

---
+++ arithmeticOperators.py
@@ -0,0 +1,12 @@
+x = 9
+y = 4
+print ( x + y)
+print ( x - y)
+print ( x * y)
+# prints float(x/y)
+print ( x / y)
+# prints floor(x/y)
+print ( x // y)
+print ( x % y)
+# prints x ^ y
+print ( x ** y)
---
+++ logicalOperators.py
@@ -0,0 +1,16 @@
+a = 10
+b = 20
+c = 30
+print ( a < b and b < c)
+print ( a < b or b > c)
+print (not a > b)
+
+# short circuiting, last evaluated value is printed
+x = 10
+print ( x or 20)
+y = 0
+# prints 30 as y is false
+print ( y or 30)
+z = 40
+# prints 50 as z is true
+print (z and 50)
