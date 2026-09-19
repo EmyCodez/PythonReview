23a5dbf96ddc338cb57f29faf4f533c30a29324a
EmyCodez <130977482+EmyCodez@users.noreply.github.com>
2026-09-19 13:22:27 +0400

    Swap numbers and find the day before programs

---
+++ dayBefore.py
@@ -0,0 +1,4 @@
+d = int(input("Enter the day d:"))
+n = int(input("Enter no of days n:"))
+res = (d - n) % 7
+print("The day is", res)
---
+++ swap.py
@@ -0,0 +1,20 @@
+x = "geeks"
+y = "for"
+print(x)
+print(y)
+z = x
+x = y
+y = z
+print(x)
+print(y)
+a = 100
+b = 200
+# Comma Assignment
+a, b = b, a
+print(a)
+print(b)
+x, y, z = 100, "Geeks", 10.5
+print(x, y, z)
+x, y = x-5, y + "for"
+print("x =", x)
+print(y)
